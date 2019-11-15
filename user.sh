5a6fd3195b2a5b9baf05cbb6368d3123c709cc04
James Vaughn <v0rngit@gmail.com>
2019-11-15 18:32:37 +0000

    Symlink Firefox configuration directory for OpenWebStart

--- user.sh
+++ user.sh
@@ -5,4 +5,9 @@ cd "$(dirname "$0")"  # Change to script's directory
 # Copy dotfiles
 gcp -r --dereference dotfiles/. "$HOME"
 
+# Symlink Firefox profile directory for Java Web Start (used only for BMC
+# console redirection)
+mkdir "$HOME/.mozilla/" 2> /dev/null
+ln -s "$HOME/Library/Application Support/Firefox/" "$HOME/.mozilla/firefox" 2> /dev/null
+
 # vim: set filetype=bash:
