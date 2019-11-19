3ffe346ced0d8fa837ec8c29413296b7a4f7ded3
James Vaughn <v0rngit@gmail.com>
2019-11-19 16:41:58 +0000

    WM: Set default Finder window size per aspect ratio

---
+++ dotfiles/.yabai/float_default_size.sh
@@ -0,0 +1,10 @@
+#!/usr/bin/env sh
+
+ratio=$(($(yabai -m query --displays --display | jq -r '.frame | del(.x, .y) | join("/")')))
+
+case $ratio in
+  1) yabai -m window --grid 10:10:1:1:8:7 ;;  # Normal aspect ratios, e.g. 16:10
+  2) yabai -m window --grid 10:10:3:1:4:6 ;;  # Ultrawide, e.g. 21:9
+esac
+
+# vim: set filetype=bash:
