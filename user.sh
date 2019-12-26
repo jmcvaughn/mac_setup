c4064dd7535c9957967b2b361ae3ba5212a87ba5
James Vaughn <v0rngit@gmail.com>
2019-12-26 17:26:04 +0000

    Add iTerm2 Zsh shell integration

--- user.sh
+++ user.sh
@@ -5,6 +5,9 @@ cd "$(dirname "$0")"  # Change to script's directory
 # Copy dotfiles
 gcp -r --dereference dotfiles/. "$HOME"
 
+# Fetch iTerm2 Zsh shell integration script
+curl -L https://iterm2.com/shell_integration/zsh -o "$HOME"/.iterm2_shell_integration.zsh
+
 # Symlink Firefox profile directory for Java Web Start (used only for BMC
 # console redirection)
 gmkdir "$HOME/.mozilla/" 2> /dev/null
