0832bf9fd8388964d4cdaaf054048844c7554b87
James Vaughn <v0rngit@gmail.com>
2019-12-26 17:04:41 +0000

    Use GNU utilities in user configuration script

--- user.sh
+++ user.sh
@@ -7,7 +7,7 @@ gcp -r --dereference dotfiles/. "$HOME"
 
 # Symlink Firefox profile directory for Java Web Start (used only for BMC
 # console redirection)
-mkdir "$HOME/.mozilla/" 2> /dev/null
-ln -s "$HOME/Library/Application Support/Firefox/" "$HOME/.mozilla/firefox" 2> /dev/null
+gmkdir "$HOME/.mozilla/" 2> /dev/null
+gln -s "$HOME/Library/Application Support/Firefox/" "$HOME/.mozilla/firefox" 2> /dev/null
 
 # vim: set filetype=bash:
