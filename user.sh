4fa4cd104782ecac5d1b74bb3148c75a78e221fc
James Vaughn <v0rngit@gmail.com>
2020-02-28 11:36:33 +0000

    POSIX-compliant redirection, supress ~/vagrant/ creation

--- user.sh
+++ user.sh
@@ -5,14 +5,14 @@ cd "$(dirname "$0")"  # Change to script's directory
 # Copy dotfiles
 gcp -r --dereference dotfiles/. "$HOME"
 
-mkdir "$HOME"/vagrant/
+gmkdir "$HOME"/vagrant/ > /dev/null 2>&1
 
 # Fetch iTerm2 Zsh shell integration script
 curl -L https://iterm2.com/shell_integration/zsh -o "$HOME"/.iterm2_shell_integration.zsh
 
 # Symlink Firefox profile directory for Java Web Start (used only for BMC
 # console redirection)
-gmkdir "$HOME"/.mozilla/ 2> /dev/null
-gln -s "$HOME/Library/Application Support/Firefox/" "$HOME"/.mozilla/firefox 2> /dev/null
+gmkdir "$HOME"/.mozilla/ > /dev/null 2>&1
+gln -s "$HOME/Library/Application Support/Firefox/" "$HOME"/.mozilla/firefox > /dev/null 2>&1
 
 # vim: set filetype=bash:
